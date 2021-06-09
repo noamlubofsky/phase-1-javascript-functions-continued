37a4128af45880edca9301197d5aae26fa288dd0
github username <github email address>
2021-06-09 11:27:08 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(wrapper = '*') {
+    return function (string = `special`) {
+     return (`You are ${wrapper + string + wrapper}!`)
+ }
+ 
+}
+
